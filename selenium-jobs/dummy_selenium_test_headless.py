3aae6103590274cf24d68baa099946c549e2429e
ozihan <ozihanal@gmail.com>
2022-03-11 15:23:20 +0000

    added scripts for running dummy selenium job2

--- selenium-jobs/dummy_selenium_test_headless.py
+++ selenium-jobs/dummy_selenium_test_headless.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("headless")
+# chrome_options.add_argument("headless")
 chrome_options.add_argument("no-sandbox")
 chrome_options.add_argument("disable-dev-shm-usage")
 driver = webdriver.Chrome(options=chrome_options)
